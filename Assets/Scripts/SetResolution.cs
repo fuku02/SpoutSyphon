9397e359c589d42567f37c07d0e4aaad43e04602
fks <fuku02@gmail.com>
2021-05-12 10:17:54 +0900

    整理

--- Assets/Scripts/SetResolution.cs
+++ Assets/Scripts/SetResolution.cs
@@ -17,7 +17,8 @@ using Klak.Syphon; // need this to declare SyphonServer
 ************************************************************/
 public class SetResolution : MonoBehaviour
 {
-    enum IMAGE_QUALITY{
+    enum IMAGE_QUALITY
+    {
         VERY_LOW,
         LOW,
         MID,
@@ -25,65 +26,69 @@ public class SetResolution : MonoBehaviour
         VERY_HIGH,
         ULTRA,
     };
-	
-	[SerializeField] SyphonServer SyphonServer;
-	readonly Vector2 resolution = new Vector2(1280, 720);
-	
-	bool b_ResolutionSet = false;
-	
-	enum STATE{
-		WAIT__SET_RESOLUTION,
-		COMP__SET_RESOLUTION,
-	};
-	STATE State = STATE.WAIT__SET_RESOLUTION;
-    
-	/******************************
+
+    [SerializeField] SyphonSender syphonSender;
+    readonly Vector2 resolution = new Vector2(1280, 720);
+
+    bool b_ResolutionSet = false;
+
+    enum STATE
+    {
+        WAIT__SET_RESOLUTION,
+        COMP__SET_RESOLUTION,
+    };
+    STATE State = STATE.WAIT__SET_RESOLUTION;
+
+    /******************************
 	******************************/
     void Awake()
     {
-		/********************
+        /********************
 		Screen.SetResolution()は別threadで実行され、完了までに時間が掛かる(環境依存だが、1sec - )。
 		********************/
-		QualitySettings.SetQualityLevel((int)IMAGE_QUALITY.HIGH, true/* applyExpensiveChanges */);
-		Screen.SetResolution((int)resolution.x, (int)resolution.y, false/* full screen */);
-		
-		/********************
+        QualitySettings.SetQualityLevel((int)IMAGE_QUALITY.HIGH, true/* applyExpensiveChanges */);
+        Screen.SetResolution((int)resolution.x, (int)resolution.y, false/* full screen */);
+
+        /********************
 		Coroutineを使って、SetResolutionの完了をjudge.
 		********************/
-		SyphonServer.enabled = false;
-		StartCoroutine(_IsSetResolution_OK(resolution));
-	}
-	/******************************
+        syphonSender.enabled = false;
+        StartCoroutine(_IsSetResolution_OK(resolution));
+    }
+    /******************************
 	******************************/
     void Start()
     {
     }
 
-	/******************************
-	******************************/ 
+    /******************************
+	******************************/
     void Update()
     {
-		/********************
+        /********************
 		********************/
-		switch(State){
-			case STATE.WAIT__SET_RESOLUTION:
-				if(b_ResolutionSet){
-					State = STATE.COMP__SET_RESOLUTION;
-					SyphonServer.enabled = true;
-				}
-				break;
-				
-			case STATE.COMP__SET_RESOLUTION:
-				break;
-		
-		}
+        switch (State)
+        {
+            case STATE.WAIT__SET_RESOLUTION:
+                if (b_ResolutionSet)
+                {
+                    State = STATE.COMP__SET_RESOLUTION;
+                    syphonSender.enabled = true;
+                }
+                break;
+
+            case STATE.COMP__SET_RESOLUTION:
+                break;
+
+        }
     }
-	
-	/****************************************
+
+    /****************************************
 	****************************************/
-	IEnumerator _IsSetResolution_OK(Vector2 targetResolution) {
-		yield return new WaitUntil(() => Screen.width == (int)targetResolution.x && Screen.height == (int)targetResolution.y);
-		
-		b_ResolutionSet = true;
-	}
+    IEnumerator _IsSetResolution_OK(Vector2 targetResolution)
+    {
+        yield return new WaitUntil(() => Screen.width == (int)targetResolution.x && Screen.height == (int)targetResolution.y);
+
+        b_ResolutionSet = true;
+    }
 }
